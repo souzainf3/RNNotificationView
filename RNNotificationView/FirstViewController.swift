9b148f2a2c4ba222e8ed7e8aee6010426db10f79
Romilson Nunes <souzainf3@yahoo.com.br>
2016-10-21 12:24:06 -0200

    Project started

---
+++ RNNotificationView/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RNNotificationView
+//
+//  Created by Romilson Nunes on 21/10/16.
+//  Copyright © 2016 Romilson Nunes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RNNotificationView/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RNNotificationView
+//
+//  Created by Romilson Nunes on 21/10/16.
+//  Copyright © 2016 Romilson Nunes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
