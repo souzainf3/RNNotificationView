10e53f6ad1af3a4434a652723e454e3ebbccd652
Romilson Nunes <souzainf3@yahoo.com.br>
2016-10-21 14:19:53 -0200

    Project Demo

---
+++ Example/RNNotificationViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RNNotificationViewDemo
+//
+//  Created by Romilson Nunes on 21/10/16.
+//  Copyright © 2016 Romilson Nunes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
